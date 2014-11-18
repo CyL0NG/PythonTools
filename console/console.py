7e56136211e550c04ce1ea3d78f1f844bdfd37ce
cylong2013 <cuiyulong2011@gmail.com>
2014-11-18 15:05:46 +0800

    change comment

--- console/console.py
+++ console/console.py
@@ -12,27 +12,27 @@ class Console(object):
     def __init__(self):
         if platform.system() == "Windows":
             self._platform = "windows"
-            self._color = {'danger': { 'color': 0x0c, 'symbol': '-'}, #red
-                    'warning': { 'color': 0x0e, 'symbol': '-'}, #yellow
-                    'success': { 'color': 0x0a, 'symbol': '+'}, #green
-                    'info': {'color': 0x0f, 'symbol': '*'}  #white
+            self._color = {'danger': { 'color': 0x0c, 'symbol': '-'},  # red
+                    'warning': { 'color': 0x0e, 'symbol': '-'},  # yellow
+                    'success': { 'color': 0x0a, 'symbol': '+'},  # green
+                    'info': {'color': 0x0f, 'symbol': '*'}   # white
                     }
             self._handler = ctypes.windll.kernel32.GetStdHandle(-11)
             self._process = self.__print_Windows
         else:
-            self._color = {'danger': {'color': '\033[22;31m', 'symbol': '-'}, #red
-                    'warning': {'color': '\033[01;33m', 'symbol': '-'}, #yellow
-                    'success': {'color': '\033[22;32m', 'symbol': '+'}, #green
-                    'info': {'color': '\033[01;37m', 'symbol': '*'}, #white
-                    'default': {'color': '\033[0m', 'symbol': ''}#default
+            self._color = {'danger': {'color': '\033[22;31m', 'symbol': '-'},  # red
+                    'warning': {'color': '\033[01;33m', 'symbol': '-'},  # yellow
+                    'success': {'color': '\033[22;32m', 'symbol': '+'},  # green
+                    'info': {'color': '\033[01;37m', 'symbol': '*'},  # white
+                    'default': {'color': '\033[0m', 'symbol': ''}  # default
                     }
             self._process = self.__print_Linux
             
     def __print_Windows(self, msg, type):
-        #set console color
+        # set console color
         ctypes.windll.kernel32.SetConsoleTextAttribute(self._handler, self._color[type]['color'])
         print '[%s] %s' % (self._color[type]['symbol'], msg)
-        #reset color
+        # reset color
         ctypes.windll.kernel32.SetConsoleTextAttribute(self._handler, self._color['info']['color'])
 
     def __print_Linux(self, msg, type):
